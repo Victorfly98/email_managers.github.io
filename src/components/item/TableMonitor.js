0bc8bd7526063d2f04191ecb709458a60e5a330f
Victorfly <bboyquangvinh1998@gmail.com>
2020-03-16 16:36:02 +0700

    fix 16/03

--- src/components/item/TableMonitor.js
+++ src/components/item/TableMonitor.js
@@ -1,104 +1,157 @@
-import React from 'react';
-import { Table, Popconfirm } from 'antd';
+import React from "react";
+import { Table, Popconfirm, Button } from "antd";
+// import _ from "lodash";
 
 export default class EditableTable extends React.Component {
   constructor(props) {
     super(props);
     this.columns = [
-        [
-            {
-                title: 'name',
-                dataIndex: 'name',
-                width: '30%',
-            },
-            {
-                title: 'age',
-                dataIndex: 'username',
-            },
-            {
-                title: 'address',
-                dataIndex: 'email',
-            },
-            {
-                title: 'operation',
-                dataIndex: 'operation',
-                render: (text, record) =>
-                this.state.dataSource.length >= 1 ? (
-                    <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.id)}>
-                    <a>Delete</a>
-                    </Popconfirm>
-                ) : null,
-            },
-        ],
-        [
-          {
-              title: 'abc',
-              dataIndex: 'email',
-              width: '30%',
-          },
-          {
-              title: 'xyz',
-              dataIndex: 'username',
-          },
-          {
-              title: '123',
-              dataIndex: 'email',
-          },
-          {
-              title: 'operation',
-              dataIndex: 'operation',
-              render: (text, record) =>
-              this.state.dataSource.length >= 1 ? (
-                  <Popconfirm title="Sure to delete?" onConfirm={() => this.handleDelete(record.id)}>
-                  <a>Delete</a>
-                  </Popconfirm>
-              ) : null,
-          },
+      [
+        {
+          title: "Recipient",
+          dataIndex: "name"
+        },
+        {
+          title: "Timestamp",
+          dataIndex: "username"
+        },
+        {
+          title: "Description",
+          dataIndex: "email"
+        },
+        {
+          title: " ",
+          dataIndex: "operation",
+          render: (text, record) =>
+            this.state.dataSource.length >= 1 ? (
+              <Popconfirm
+                title="Sure to delete?"
+                onConfirm={() => this.handleDelete(record.id)}
+              >
+                <Button
+                  style={{
+                    borderRadius: 16,
+                    backgroundColor: "#f7e9e9",
+                    color: "#c02428"
+                  }}
+                >
+                  Delete
+                </Button>
+              </Popconfirm>
+            ) : null
+        }
+      ],
+      [
+        {
+          title: "abc",
+          dataIndex: "email",
+          width: "30%"
+        },
+        {
+          title: "xyz",
+          dataIndex: "username"
+        },
+        {
+          title: "123",
+          dataIndex: "email"
+        },
+        {
+          title: "operation",
+          dataIndex: "operation",
+          render: (text, record) =>
+            this.state.dataSource.length >= 1 ? (
+              <Popconfirm
+                title="Sure to delete?"
+                onConfirm={() => this.handleDelete(record.id)}
+              >
+                <Button
+                  style={{
+                    borderRadius: 16,
+                    backgroundColor: "#f7e9e9",
+                    color: "#c02428"
+                  }}
+                >
+                  Delete
+                </Button>
+              </Popconfirm>
+            ) : null
+        }
       ]
     ];
     this.state = {
       dataSource: [],
       update: 0,
-      columns: []
+      columns: [],
+      selectedRowKeys: []
     };
   }
 
   handleDelete = id => {
     const dataSource = [...this.state.dataSource];
     this.setState({
-      dataSource: dataSource.filter(item => item.id !== id),
+      dataSource: dataSource.filter(item => item.id !== id)
     });
   };
-  
+  onSelectChange = selectedRowKeys => {
+    console.log("selectedRowKeys changed: ", selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  };
+
   render() {
-    if(this.state.update < 2){
-    //    console.log('abc: ',this.props[0])
-        this.state.dataSource = this.props[0]
-        this.state.update +=1
-        this.state.columns = this.columns[this.props.type]
+    if (this.state.update < 2) {
+      this.state.dataSource = this.props[0];
+      this.state.update += 1;
+      this.state.columns = this.columns[this.props.type];
     }
     const { dataSource } = this.state;
     const columns = this.state.columns.map(col => {
       if (!col.editable) {
         return col;
       }
-
       return {
         ...col,
         onCell: record => ({
           record,
-        //  dataIndex: col.dataIndex,
-          title: col.title,
-        }),
+          //  dataIndex: col.dataIndex,
+          title: col.title
+        })
       };
     });
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange
+    };
+    const data = dataSource.map(e => {
+      return {
+        key: e["id"],
+        ...e  
+      };
+    });
+    console.log(data, "data");
+    this.state.dataSource = data;
+    console.log(dataSource, 'dataSource');
+    
+    const hasSelected = selectedRowKeys.length > 0;
     return (
       <div>
+        <Button
+          disabled={!hasSelected}
+          style={{
+            borderRadius: 16,
+            backgroundColor: "#f7e9e9",
+            color: "#c02428",
+            marginBottom: "20"
+          }}
+        >
+          Remove Selected
+        </Button>
         <Table
-          rowClassName={() => 'editable-row'}
-          bordered
+          rowClassName={() => "editable-row"}
+          // bordered
           dataSource={dataSource}
           columns={columns}
+          rowSelection={rowSelection}
         />
       </div>
     );
--- src/components/screen/EmailMonitoring.js
+++ src/components/screen/EmailMonitoring.js
@@ -1,51 +1,60 @@
-import React, { Component } from 'react'
-
-import { connect } from 'react-redux'
-import { Tabs } from 'antd';
-import TableMonitor from '../item/TableMonitor'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Tabs, Typography, Input } from "antd";
+import TableMonitor from "../item/TableMonitor";
 import getAction from "../../redux/action";
 
+const { Title } = Typography;
+const { Search } = Input;
 const { TabPane } = Tabs;
 
 class EmailMonitoring extends Component {
-    
-    componentDidMount = () => {
-        this.props.getMonitorEmail()
-    }
+  componentDidMount = () => {
+    this.props.getMonitorEmail();
+  };
 
-    render() {
+  render() {
     //    console.log(this.props.monitor.monitorEmail)
-        return (
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="Tab 1" key="1">
-                    <TableMonitor {...[this.props.monitor.monitorEmail]} type = '0'></TableMonitor>
-                </TabPane>
-                <TabPane tab="Tab 2" key="2">
-                    <TableMonitor {...[this.props.monitor.monitorEmail]} type = '1'></TableMonitor>
-                </TabPane>
-                <TabPane tab="Tab 3" key="3">
-                    <TableMonitor></TableMonitor>
-                </TabPane>
-            </Tabs>
-        )
-    }
+    return (
+      <div>
+        <Title level={3}>EMAIL MONITORING</Title>
+        <Search
+          placeholder="Seach for recipient"
+          onSearch={value => console.log(value)}
+          style={{ width: 200 }}
+        />
+        <Tabs defaultActiveKey="bounces">
+          <TabPane tab="Bounces" key="bounces">
+            <TableMonitor
+              {...[this.props.monitor.monitorEmail]}
+              type="0"
+            ></TableMonitor>
+          </TabPane>
+          <TabPane tab="Complaints" key="complaint">
+            <TableMonitor
+              {...[this.props.monitor.monitorEmail]}
+              type="1"
+            ></TableMonitor>
+          </TabPane>
+          <TabPane tab="Unsubscribes" key="unsub"></TabPane>
+        </Tabs>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        monitor: state.reducer
-    }
-}
+  return {
+    monitor: state.reducer
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-       getMonitorEmail: () => {
-           dispatch(getAction.action.getMonitorEmail())
-       }
+  return {
+    getMonitorEmail: () => {
+      dispatch(getAction.action.getMonitorEmail());
     }
-}
+  };
+};
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(EmailMonitoring)
+export default connect(mapStateToProps, mapDispatchToProps)(EmailMonitoring);
