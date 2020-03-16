fb6dd15149a6dd31a24c92840eee2bc7cc399cfc
Victorfly <bboyquangvinh1998@gmail.com>
2020-03-16 17:20:41 +0700

    update

--- src/components/screen/ExcelReader.js
+++ src/components/screen/ExcelReader.js
@@ -3,13 +3,13 @@ import _ from "lodash";
 import { SheetJSFT } from "../readExcelFile/types.js";
 import XLSX from "xlsx";
 import { InputLabel } from "@material-ui/core";
-import { Input, Table, Button, Select, Upload, Form } from "antd";
+import { Input, Table, Button, Select, Upload, Form, Typography, Row, Col } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import getAction from "../../redux/action";
 import { connect } from "react-redux";
 const { Option } = Select;
-
+const { Title } = Typography;
 const { TextArea } = Input;
 const columns = [
   {
@@ -99,12 +99,12 @@ class ExcelReader extends React.Component {
       }
       return file;
     });
-    let {buffer} = this.state
-    buffer = []
-    fileList.map( vl => {
-      this.transformFile(vl.originFileObj).then( function(v) {
+    let { buffer } = this.state;
+    buffer = [];
+    fileList.map(vl => {
+      this.transformFile(vl.originFileObj).then(function(v) {
         buffer.push(new Uint8Array(v));
-      //  console.log('v: ',v); // 1
+        //  console.log('v: ',v); // 1
       });
     });
     this.setState({ fileList });
@@ -115,7 +115,7 @@ class ExcelReader extends React.Component {
   transformFile = file => {
     const temporaryFileReader = new FileReader();
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
       temporaryFileReader.onerror = () => {
         temporaryFileReader.abort();
         reject(new DOMException("Problem parsing input file."));
@@ -124,7 +124,6 @@ class ExcelReader extends React.Component {
       temporaryFileReader.onload = async () => {
         resolve(temporaryFileReader.result);
       };
-      
     });
   };
 
@@ -137,29 +136,34 @@ class ExcelReader extends React.Component {
     });
   }
   onSelectChange = selectedRowKeys => {
-   const y = this.state.data;
+    const y = this.state.data;
     this.state.Email = [];
     selectedRowKeys.map(e => {
       if (e.type === undefined) {
         let type;
-        let index = y.findIndex(vl => vl.email === e)
-        if (index === -1) { type = 'a'; console.log('true') }
-        else { type = y[index].type; console.log('false') }
+        let index = y.findIndex(vl => vl.email === e);
+        if (index === -1) {
+          type = "a";
+          console.log("true");
+        } else {
+          type = y[index].type;
+          console.log("false");
+        }
         e = {
           Email: e,
           type_id: type
-        }
+        };
       }
       this.state.Email.push(e);
     });
     this.setState({ selectedRowKeys });
     // this.setState({showEmail: selectedRowKeys})
-   console.log("showEmail: ", this.state.Email);
+    console.log("showEmail: ", this.state.Email);
   };
 
   // function send mail
   onSendMail = () => {
-  //  console.log("item:  ", this.state.fileList);
+    //  console.log("item:  ", this.state.fileList);
     // this.state.fileList.map( vl => {
     // //  console.log('x: ',x)
     //   this.state.fileName.push(vl.name);
@@ -167,19 +171,19 @@ class ExcelReader extends React.Component {
     let customers = this.state.Email;
     let subject = this.state.subject;
     let message = this.state.message;
-    console.log(this.state, ': this.state')
-    let formData = new FormData()
-    formData.append('subject', subject);
-    formData.append('text', message);
-    formData.append('list_customers', JSON.stringify(customers));
-    console.log(this.state.fileName, ": this.state.fileName")
+    console.log(this.state, ": this.state");
+    let formData = new FormData();
+    formData.append("subject", subject);
+    formData.append("text", message);
+    formData.append("list_customers", JSON.stringify(customers));
+    console.log(this.state.fileName, ": this.state.fileName");
     // this.state.fileName.forEach((name) => {
     //   formData.append('file_name', name);
     // })
-    this.state.fileList.forEach((file) => {
-      formData.append('files', file.originFileObj, file.name);
-    })
-    console.log(formData, ': formdata')
+    this.state.fileList.forEach(file => {
+      formData.append("files", file.originFileObj, file.name);
+    });
+    console.log(formData, ": formdata");
     this.props.sendMail(formData);
   };
   render() {
@@ -199,57 +203,65 @@ class ExcelReader extends React.Component {
 
     return (
       <Form>
-        <div className="excel_reader">
-          GỬI MAIL
-          <Form.Item
-            label="Chọn file chứa thông tin khách hàng:"
-            name="chose_file"
-          >
-            <Input
-              type="file"
-              className="form-control"
-              id="file"
-              accept={SheetJSFT}
-              onChange={this.handleChange}
-            />
-          </Form.Item>
-          <Button
-            disabled={!this.state.file}
-            type="submit"
-            className="btn_import"
-            onClick={this.handleFile}
-          >
-            Import
-          </Button>
-        </div>
-        <InputLabel style={{ marginTop: 20 }}>Loại khách hàng: </InputLabel>
-        <Select
-          style={{ width: 170, textAlign: "center" }}
-          value={this.state.selected}
-          onChange={this.onChange}
+        <Title level = {3}>SEND MAIL</Title>
+        <Form.Item
+          label="Chọn file chứa thông tin khách hàng:"
+          name="chose_file"
         >
-          <Option value="all">Tất cả</Option>
-          {_.map(groups, g => {
-            return (
-              <Option key={g} value={g}>
-                {g}
-              </Option>
-            );
-          })}
-        </Select>
-        {this.state.selected === "all" ? (
-          <Table
-            rowSelection={rowSelection}
-            columns={columns}
-            dataSource={this.state.data}
-          ></Table>
-        ) : (
+          <Row>
+            <Col span="9">
+              <Input
+                type="file"
+                className="form-control"
+                id="file"
+                accept={SheetJSFT}
+                onChange={this.handleChange}
+                style={{width: 300}}
+              />
+            </Col>
+            <Col span="15">
+              <Button
+                disabled={!this.state.file}
+                type="submit"
+                className="btn_import"
+                onClick={this.handleFile}
+              >
+                Import
+              </Button>
+            </Col>
+          </Row>
+        </Form.Item>
+        <Form.Item label="Lọc loại khách hàng: " name="type_customer">
+          <Select
+            style={{ width: 170, textAlign: "center" }}
+            value={this.state.selected}
+            onChange={this.onChange}
+          >
+            <Option value="all">Tất cả</Option>
+            {_.map(groups, g => {
+              return (
+                <Option key={g} value={g}>
+                  {g}
+                </Option>
+              );
+            })}
+          </Select>
+        </Form.Item>  
+        <Form.Item name="table_customer">
+          {this.state.selected === "all" ? (
+            <Table
+              rowSelection={rowSelection}
+              columns={columns}
+              dataSource={this.state.data}
+            ></Table>
+          ) : (
             <Table
               rowSelection={rowSelection}
               columns={columns}
               dataSource={this.state.filter}
             ></Table>
           )}
+        </Form.Item>
         <div
           className="content_email"
           style={{
