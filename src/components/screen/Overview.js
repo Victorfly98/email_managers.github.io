945012b2a8781fe9b5e9a4b24031aa33294f6e9a
Victorfly <bboyquangvinh1998@gmail.com>
2020-03-24 15:06:30 +0700

    update type highchart

--- src/components/screen/Overview.js
+++ src/components/screen/Overview.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import getAction from "../../redux/action";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { Typography } from "antd";
+import { Typography, Switch } from "antd";
 //import HighchartsReact
 import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
-import moment from 'moment';
+import moment from "moment";
 // import HC_more from "highcharts/highcharts-more"; //module
 // HC_more(Highcharts); //init module
 
@@ -23,6 +23,7 @@ class Overview extends Component {
       labelDelivered: [],
       labelBonces: [],
       categories: [],
+      type: "spline"
     };
     this.toggleDataSeries = this.toggleDataSeries.bind(this);
   }
@@ -44,16 +45,16 @@ class Overview extends Component {
   };
   // test server
   convertData(dataConvert) {
-    dataConvert.sort(function (a, b) {
+    dataConvert.sort(function(a, b) {
       return a.time - b.time;
     });
     let groups = _.groupBy(dataConvert, "date");
-    this.setState({ categories: Object.keys(groups) })
-    console.log(groups,'groups')
-    var result = Object.keys(groups).map(function (key) {
+    this.setState({ categories: Object.keys(groups) });
+    console.log(groups, "groups");
+    var result = Object.keys(groups).map(function(key) {
       return [key, groups[key]];
     });
-    console.log(result,'result')
+    console.log(result, "result");
     let data = [];
     result.map(vl => {
       data.push([
@@ -61,8 +62,8 @@ class Overview extends Component {
         vl[1].length
       ]);
     });
-    console.log(data, 'data')
-    return data
+    console.log(data, "data");
+    return data;
   }
 
   componentWillReceiveProps(nextprops) {
@@ -86,64 +87,71 @@ class Overview extends Component {
         };
       });
       // set state data bonces
-      const dataBonces = this.convertData(dataBoncesEmail)
+      const dataBonces = this.convertData(dataBoncesEmail);
       // set state data delivered
-      const dataDelivered = this.convertData(dataDeliveredEmail)
+      const dataDelivered = this.convertData(dataDeliveredEmail);
 
       this.setState({ dataBonces: dataBonces, dataDelivered: dataDelivered });
     }
   }
+
+  onChange = checked => {
+    if (checked) {
+      this.setState({ type: "column" });
+    } else {
+      this.setState({ type: "spline" });
+    }
+  };
   render() {
     const { dataBonces, dataDelivered } = this.state;
-    console.log(dataDelivered, 'dataDelivered');
-    console.log(dataBonces, 'dataBonces');
-    console.log(this.state.categories, 'this.state.categories');
-    
-
+    console.log(dataDelivered, "dataDelivered");
+    console.log(dataBonces, "dataBonces");
+    console.log(this.state.categories, "this.state.categories");
     const optionsData = {
       chart: {
-        type: "spline"
+        type: this.state.type
       },
       credits: {
         enabled: false
       },
       title: {
-        text: 'MAIL STATISTICS'
+        text: "MAIL STATISTICS"
       },
-      
 
       xAxis: {
-        type: 'datetime',
+        type: "datetime",
         labels: {
-          format: '{value:%b %e \%Y}',
+          format: "{value:%b %e %Y}",
           rotation: -45,
-          align: 'right'
+          align: "right"
         }
       },
       yAxis: {
         title: {
-          text: 'Number of email'
+          text: "Number of email"
         }
       },
       series: [
         {
           name: "Bounces",
-          type: 'spline',
-          data: dataBonces,
-        }, {
-          name: "Delivered",
-          type: 'spline',
-          data: dataDelivered
+          data: dataBonces
         },
+        {
+          name: "Delivered",
+          data: dataDelivered
+        }
       ]
     };
     return (
       <div style={{ width: 1000 }}>
         <Title level={3}>OVERVIEW</Title>
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={optionsData}
+        <Switch
+          style={{ textAlign: "center" }}
+          checkedChildren="Spline"
+          unCheckedChildren="Bar"
+          onChange={this.onChange}
         />
+        <HighchartsReact highcharts={Highcharts} options={optionsData} />
       </div>
     );
   }
